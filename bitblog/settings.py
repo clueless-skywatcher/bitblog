48fda30a37612c93facc41634c0c40d590d978b0
clueless-skywatcher <somichat@gmail.com>
2021-04-16 15:43:30 +0530

    Updated sigils branch to include PostgreSQL db

--- bitblog/settings.py
+++ bitblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -83,8 +84,12 @@ ASGI_APPLICATION = 'bitblog.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': 5432
     }
 }
 
